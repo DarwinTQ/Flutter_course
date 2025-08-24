2550b812ee41eba6ec8b033981511fff2b8669c3
darwin <dturpoqu@unsa.edu.pe>
2025-08-24 13:07:51 -0500

    Hello world flutter

---
+++ hello_world_app/lib/presentation/screens/counters/counter_sceen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CounterSceen extends StatelessWidget {
+  const CounterSceen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(body: Center(child: Text('Counter screen')));
+  }
+}
